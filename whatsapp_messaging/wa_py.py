e8787d1105bf46df054dba52cabaf9aa6d0c9707
sofiaiotti <sofiaiotti1998@gmail.com>
2025-04-18 14:27:50 +0200

    whatsapp messaging with pywhatkit

---
+++ whatsapp_messaging/wa_py.py
@@ -0,0 +1,7 @@
+import pywhatkit
+
+#Easy method to send automatized whatsapp messages to a number: sendwhatmsg(number, text, h, m)
+pywhatkit.sendwhatmsg("+393313814403", 
+                      "Funziona!", 
+                      14, 23)
+
